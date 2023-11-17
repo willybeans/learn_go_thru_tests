d4aab5408abb89ccccd2e52a74abc5bbf589ddeb
Will Wedmedyk <willwedmedyk@gmail.com>
2023-11-17 12:15:46 -0500

    ww/mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -14,14 +14,23 @@ type Sleeper interface {
 	Sleep()
 }
 
-type DefaultSleeper struct{}
+// type DefaultSleeper struct{}
 
-func (d *DefaultSleeper) Sleep() {
-	time.Sleep(1 * time.Second)
+// func (d *DefaultSleeper) Sleep() {
+// 	time.Sleep(1 * time.Second)
+// }
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
 }
 
 func main() {
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
 
